4d813fea664dd6f9a9a3d796090d61575ba0a8e3
TienDatCactus <multivncraft@gmail.com>
2026-06-29 15:30:22 +0700

    fix(aas): reformat based on codeql checks

--- apps/api/src/mail/mail.module.ts
+++ apps/api/src/mail/mail.module.ts
@@ -1,31 +1,31 @@
-import { MailerModule } from '@nestjs-modules/mailer';
-import { Module } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { MailService } from './mail.service';
-import { MailController } from './mail.controller';
-import * as dns from 'dns';
+import { MailerModule } from "@nestjs-modules/mailer";
+import { Module } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { MailService } from "./mail.service";
+import { MailController } from "./mail.controller";
+import * as dns from "dns";
 @Module({
   imports: [
     MailerModule.forRootAsync({
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         transport: {
-          host: configService.get('email.host'),
-          port: configService.get('email.port'),
-          secure: configService.get('email.secure'),
+          host: configService.get("email.host"),
+          port: configService.get("email.port"),
+          secure: configService.get("email.secure"),
           auth: {
-            user: configService.get('email.user'),
-            pass: configService.get('email.pass'),
+            user: configService.get("email.user"),
+            pass: configService.get("email.pass"),
           },
           lookup: (hostname, options, callback) => {
             dns.lookup(hostname, { family: 4 }, callback);
           },
           tls: {
-            rejectUnauthorized: false,
+            rejectUnauthorized: true,
           },
         },
         defaults: {
-          from: '"No Reply" <' + configService.get('email.user') + '>',
+          from: '"No Reply" <' + configService.get("email.user") + ">",
         },
       }),
     }),
--- packages/transactional/src/emails/welcome.tsx
+++ packages/transactional/src/emails/welcome.tsx
@@ -1,7 +1,6 @@
 // Get the full source code, including the theme and Tailwind config:
 // https://github.com/resend/react-email/tree/canary/apps/demo/emails
 
-import { Body } from "react-email";
 import EmailLayout from "./_components/layout";
 import { baseURL } from "./utils";
 
@@ -9,16 +8,13 @@ export interface WelcomeEmailProps {
   url?: string;
 }
 const companyName = "All About SaaS";
-export const WelcomeEmail = ({
-  url = "https://example.com/",
-}: WelcomeEmailProps) => {
+export const WelcomeEmail = ({ url = "https://example.com/" }: WelcomeEmailProps) => {
   return (
     <EmailLayout
       content={{
         title: `Welcome to ${companyName}`,
         subtitle: `Thank you for signing up for ${companyName}`,
-        legend:
-          "You're all set—explore what's new and get your first project going.",
+        legend: "You're all set—explore what's new and get your first project going.",
         brand: companyName,
         cta: {
           text: "Activate your account",
@@ -37,9 +33,5 @@ export const WelcomeEmail = ({
     />
   );
 };
-
-WelcomeEmail.PreviewProps = {
-  url: "https://example.com/",
-} satisfies WelcomeEmailProps;
 
 export default WelcomeEmail;
